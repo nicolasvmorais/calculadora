3b167f7f626d62a7b42f3474c2ef5d7f986cdd2d
nicolasvmorais <nicolasvmorais@gmail.com>
2023-11-05 22:49:15 -0300

    primeira versão (1.0)

---
+++ script.js
@@ -0,0 +1,38 @@
+function calculateNumber(){
+    const firstNumber = parseFloat(document.querySelector(".numeroEscolhido1").value)
+    const secondNumber = parseFloat(document.querySelector(".numeroEscolhido2").value)
+    const input = parseFloat(document.getElementById("sinalEscolhido").value)
+    const result = document.querySelector(".result")
+    if (input == 1) {
+        const makeSoma = (firstNumber + secondNumber)
+        result.innerHTML =(`Deu: ${makeSoma}`)
+    }
+
+        else if (input == 2) {
+            const makeSoma = (firstNumber - secondNumber)
+            result.innerHTML =(`Deu: ${makeSoma}`)
+        }
+        else if (input == 3) {
+            const makeSoma = (firstNumber * secondNumber)
+            result.innerHTML =(`Deu: ${makeSoma}`)
+        }
+        else if (input == 4) {
+            const makeSoma = (firstNumber / secondNumber)
+            result.innerHTML =(`Deu: ${makeSoma}`)
+        }
+        else {
+            result.innerHTML =(`Você escreveu algo errado,Parceiro`)
+        }
+
+
+
+    }
+function sinalMudado(){
+    calculateNumber()
+}
+
+
+
+
+
+
